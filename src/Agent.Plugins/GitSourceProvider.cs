5566454573d13cadf102550215ca87a4a277f296
Julio Barba <juliobbv@hotmail.com>
2019-06-03 21:29:21 -0400

    Fix shallow fetch by commit for two scenarios (#2283)
    * Fix fetch by commit for PR builds
    
    * Update dotnet-install scripts

--- src/Agent.Plugins/GitSourceProvider.cs
+++ src/Agent.Plugins/GitSourceProvider.cs
@@ -311,7 +311,7 @@ namespace Agent.Plugins.Repository
             bool exposeCred = StringUtil.ConvertToBoolean(executionContext.GetInput(Pipelines.PipelineConstants.CheckoutTaskInputs.PersistCredentials));
             
             // Read 'disable fetch by commit' value from the execution variable first, then from the environment variable if the first one is not set
-            bool fetchByCommit = !StringUtil.ConvertToBoolean(
+            bool fetchByCommit = GitSupportsFetchingCommitBySha1Hash && !StringUtil.ConvertToBoolean(
                 executionContext.Variables.GetValueOrDefault("VSTS.DisableFetchByCommit")?.Value ??
                 System.Environment.GetEnvironmentVariable("VSTS_DISABLEFETCHBYCOMMIT"), false);
 
@@ -795,13 +795,16 @@ namespace Agent.Plugins.Repository
             }
 
             List<string> additionalFetchSpecs = new List<string>();
+            string refFetchedByCommit = null;
+
             if (IsPullRequest(sourceBranch))
             {
                 // Build a 'fetch-by-commit' refspec iff the server allows us to do so in the shallow fetch scenario
                 // Otherwise, fall back to fetch all branches and pull request ref
                 if (fetchDepth > 0 && fetchByCommit && !string.IsNullOrEmpty(sourceVersion))
                 {
-                    additionalFetchSpecs.Add($"+{sourceVersion}:{_remoteRefsPrefix}{sourceVersion}");
+                    refFetchedByCommit = $"{_remoteRefsPrefix}{sourceVersion}";
+                    additionalFetchSpecs.Add($"+{sourceVersion}:{refFetchedByCommit}");
                 }
                 else
                 {
@@ -815,7 +818,8 @@ namespace Agent.Plugins.Repository
                 // Otherwise, use the default fetch behavior (i.e. with no refspecs)
                 if (fetchDepth > 0 && fetchByCommit && !string.IsNullOrEmpty(sourceVersion))
                 {
-                    additionalFetchSpecs.Add($"+{sourceVersion}:{_remoteRefsPrefix}{sourceVersion}");
+                    refFetchedByCommit = $"{_remoteRefsPrefix}{sourceVersion}";
+                    additionalFetchSpecs.Add($"+{sourceVersion}:{refFetchedByCommit}");
                 }
             }
 
@@ -833,7 +837,12 @@ namespace Agent.Plugins.Repository
             cancellationToken.ThrowIfCancellationRequested();
             executionContext.Progress(80, "Starting checkout...");
             string sourcesToBuild;
-            if (IsPullRequest(sourceBranch) || string.IsNullOrEmpty(sourceVersion))
+
+            if (refFetchedByCommit != null)
+            {
+                sourcesToBuild = refFetchedByCommit;
+            }
+            else if (IsPullRequest(sourceBranch) || string.IsNullOrEmpty(sourceVersion))
             {
                 sourcesToBuild = GetRemoteRefName(sourceBranch);
             }
