07afc373499f9843d05ce4539aa37ee14e2d0b5b
jahsu <45610531+jahsu-MSFT@users.noreply.github.com>
2019-12-02 09:52:28 -0800

    Fix string literal project name in the DPA V2 task (#2635)
    * update necore version
    
    * fix project name in DPA V2 task
    
    * code cleanup
    
    * code update

--- src/Agent.Plugins/PipelineArtifact/PipelineArtifactPluginV2.cs
+++ src/Agent.Plugins/PipelineArtifact/PipelineArtifactPluginV2.cs
@@ -9,6 +9,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Agent.Sdk;
 using Microsoft.TeamFoundation.Build.WebApi;
+using Microsoft.TeamFoundation.Core.WebApi;
 using Microsoft.TeamFoundation.DistributedTask.WebApi;
 using Microsoft.VisualStudio.Services.Common;
 using Microsoft.VisualStudio.Services.WebApi;
@@ -86,7 +87,7 @@ namespace Agent.Plugins.PipelineArtifact
             string defaultWorkingDirectory = context.Variables.GetValueOrDefault("system.defaultworkingdirectory").Value;
 
             targetPath = Path.IsPathFullyQualified(targetPath) ? targetPath : Path.GetFullPath(Path.Combine(defaultWorkingDirectory, targetPath));
-
+            
             bool onPrem = !String.Equals(context.Variables.GetValueOrDefault(WellKnownDistributedTaskVariables.ServerType)?.Value, "Hosted", StringComparison.OrdinalIgnoreCase);
             if (onPrem)
             {
@@ -129,6 +130,7 @@ namespace Agent.Plugins.PipelineArtifact
                 {
                     throw new ArgumentNullException("Project ID cannot be null.");
                 }
+                
                 Guid projectId = Guid.Parse(projectIdStr);
                 ArgUtil.NotEmpty(projectId, nameof(projectId));
 
@@ -173,7 +175,12 @@ namespace Agent.Plugins.PipelineArtifact
                 {
                     throw new ArgumentNullException("Project Name cannot be null.");
                 }
-                Guid projectId = Guid.Parse(projectName);
+                Guid projectId; 
+                bool isProjGuid = Guid.TryParse(projectName, out projectId);
+                if (!isProjGuid) 
+                {
+                    projectId = await GetProjectIdAsync(context, projectName);
+                }
                 int? pipelineId = null;
 
                 bool pipelineTriggeringBool = false;
@@ -191,7 +198,7 @@ namespace Agent.Plugins.PipelineArtifact
                 {
                     if (pipelineVersionToDownload == pipelineVersionToDownloadLatest)
                     {
-                        pipelineId = await this.GetPipelineIdAsync(context, pipelineDefinition, pipelineVersionToDownload, projectName, tagsInput, resultFilter, null, cancellationToken: token);
+                        pipelineId = await this.GetPipelineIdAsync(context, pipelineDefinition, pipelineVersionToDownload, projectId.ToString(), tagsInput, resultFilter, null, cancellationToken: token);
                     }
                     else if (pipelineVersionToDownload == pipelineVersionToDownloadSpecific)
                     {
@@ -199,7 +206,7 @@ namespace Agent.Plugins.PipelineArtifact
                     }
                     else if (pipelineVersionToDownload == pipelineVersionToDownloadLatestFromBranch)
                     {
-                        pipelineId = await this.GetPipelineIdAsync(context, pipelineDefinition, pipelineVersionToDownload, projectName, tagsInput, resultFilter, branchName, cancellationToken: token);
+                        pipelineId = await this.GetPipelineIdAsync(context, pipelineDefinition, pipelineVersionToDownload, projectId.ToString(), tagsInput, resultFilter, branchName, cancellationToken: token);
                     }
                     else
                     {
@@ -315,6 +322,25 @@ namespace Agent.Plugins.PipelineArtifact
             }
 
             return result;
+        }
+      
+        private async Task<Guid> GetProjectIdAsync(AgentTaskPluginExecutionContext context, string projectName)
+        {
+            VssConnection connection = context.VssConnection;
+            var projectClient = connection.GetClient<ProjectHttpClient>();
+
+            TeamProject proj = null;
+
+            try
+            {
+                proj = await projectClient.GetProject(projectName);
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException("Get project failed " + projectName + " , exception: " + ex);
+            }
+
+            return proj.Id;
         }
     }
 }
