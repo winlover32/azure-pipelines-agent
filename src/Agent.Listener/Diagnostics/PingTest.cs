ee53a70b662fed8de93082f5c92a9ad676451e28
Jessica Schumaker <jeschu@microsoft.com>
2020-02-12 12:26:48 -0500

    Properly dispose Ping (#2787)

--- src/Agent.Listener/Diagnostics/PingTest.cs
+++ src/Agent.Listener/Diagnostics/PingTest.cs
@@ -6,25 +6,27 @@ namespace Microsoft.VisualStudio.Services.Agent.Listener.Diagnostics
     {
         public bool Execute(ITerminal terminal)
         {
-            Ping ping = new Ping();
-            try
+            using (Ping ping = new Ping())
             {
-                terminal.WriteLine(string.Format("Attempt to Ping: {0} with timeout {1}", c_hostname, c_timeout));
-                PingReply pingreply = ping.Send(c_hostname, c_timeout);
-                terminal.WriteLine(string.Format("Address: {0}", pingreply.Address));
-                terminal.WriteLine(string.Format("Status: {0}", pingreply.Status));
-                terminal.WriteLine(string.Format("Round trip time: {0}", pingreply.RoundtripTime));
-
-                if (pingreply.Status != IPStatus.Success)
+                try
                 {
-                    terminal.WriteError(string.Format("Unsuccessful status response from {0}.  Verify internet connection is working", c_hostname));
+                    terminal.WriteLine(string.Format("Attempt to Ping: {0} with timeout {1}", c_hostname, c_timeout));
+                    PingReply pingreply = ping.Send(c_hostname, c_timeout);
+                    terminal.WriteLine(string.Format("Address: {0}", pingreply.Address));
+                    terminal.WriteLine(string.Format("Status: {0}", pingreply.Status));
+                    terminal.WriteLine(string.Format("Round trip time: {0}", pingreply.RoundtripTime));
+
+                    if (pingreply.Status != IPStatus.Success)
+                    {
+                        terminal.WriteError(string.Format("Unsuccessful status response from {0}.  Verify internet connection is working", c_hostname));
+                        return false;
+                    }
+                }
+                catch (PingException ex)
+                {
+                    terminal.WriteError(ex);
                     return false;
                 }
-            }
-            catch (PingException ex)
-            {
-                terminal.WriteError(ex);
-                return false;
             }
 
             return true;
