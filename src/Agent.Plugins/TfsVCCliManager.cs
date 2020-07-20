24470ba3ee08b9c7a538231519f6e9e1421ceb33
Mark Roghelia <mark.roghelia@microsoft.com>
2020-07-20 15:31:03 -0400

    Revert "made RunPorcelainCommandAsync and RunPorcelainCommandAsync wit retry logic and used them in WorkspacesAsync command (#3019)" (#3048)
    This reverts commit fb4a788825cacf3e7c8bee12b8bf77c853dac92b.
    
    Co-authored-by: maroghel <maroghel@microsoft.com>

--- src/Agent.Plugins/TfsVCCliManager.cs
+++ src/Agent.Plugins/TfsVCCliManager.cs
@@ -152,23 +152,13 @@ namespace Agent.Plugins.Repository
 
         protected Task<string> RunPorcelainCommandAsync(params string[] args)
         {
-            return RunPorcelainCommandAsync(FormatFlags.None, 0, args);
+            return RunPorcelainCommandAsync(FormatFlags.None, args);
         }
 
-        protected Task<string> RunPorcelainCommandAsync(int retriesOnFailure, params string[] args)
-        {
-            return RunPorcelainCommandAsync(FormatFlags.None, retriesOnFailure, args);
-        }
-
-        protected Task<string> RunPorcelainCommandAsync(FormatFlags formatFlags, params string[] args)
-        {
-            return RunPorcelainCommandAsync(formatFlags, 0, args);
-        }
-
-        protected async Task<string> RunPorcelainCommandAsync(FormatFlags formatFlags, int retriesOnFailure, params string[] args)
+        protected async Task<string> RunPorcelainCommandAsync(FormatFlags formatFlags, params string[] args)
         {
             // Run the command.
-            TfsVCPorcelainCommandResult result = await TryRunPorcelainCommandAsync(formatFlags, retriesOnFailure, args);
+            TfsVCPorcelainCommandResult result = await TryRunPorcelainCommandAsync(formatFlags, args);
             ArgUtil.NotNull(result, nameof(result));
             if (result.Exception != null)
             {
@@ -183,12 +173,6 @@ namespace Agent.Plugins.Repository
         }
 
         protected async Task<TfsVCPorcelainCommandResult> TryRunPorcelainCommandAsync(FormatFlags formatFlags, params string[] args)
-        {
-            var result = await TryRunPorcelainCommandAsync(formatFlags, 0, args);
-            return result;
-        }
-
-        protected async Task<TfsVCPorcelainCommandResult> TryRunPorcelainCommandAsync(FormatFlags formatFlags, int retriesOnFailure, params string[] args)
         {
             // Validation.
             ArgUtil.NotNull(args, nameof(args));
@@ -220,32 +204,6 @@ namespace Agent.Plugins.Repository
                 // TODO: Test whether the output encoding needs to be specified on a non-Latin OS.
                 try
                 {
-                    for (int attempt = 0; attempt < retriesOnFailure; attempt++)
-                    {
-                        int exitCode = await processInvoker.ExecuteAsync(
-                            workingDirectory: SourcesDirectory,
-                            fileName: "tf",
-                            arguments: arguments,
-                            environment: AdditionalEnvironmentVariables,
-                            requireExitCodeZero: false,
-                            outputEncoding: OutputEncoding,
-                            cancellationToken: CancellationToken);
-
-                        if (exitCode == 0)
-                        {
-                            return result;
-                        }
-
-                        int sleep = Math.Min(200 * (int)Math.Pow(5, attempt), 30000);
-                        ExecutionContext.Output($"Sleeping for {sleep} ms");
-                        Thread.Sleep(sleep);
-
-                        // Use attempt+2 since we're using 0 based indexing and we're displaying this for the next attempt.
-                        ExecutionContext.Output($@"Retrying. Attempt ${attempt + 2}/${retriesOnFailure}");
-
-                    }
-
-                    // Perform one last try and fail on non-zero exit code
                     await processInvoker.ExecuteAsync(
                         workingDirectory: SourcesDirectory,
                         fileName: "tf",
