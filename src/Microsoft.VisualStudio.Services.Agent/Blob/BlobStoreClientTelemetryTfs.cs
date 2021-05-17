67cd9bbc13daedb7f1a8ffc73eaba61fb82e410a
alex-peck <53013351+alex-peck@users.noreply.github.com>
2021-05-17 14:20:32 -0400

    Remove unneeded blob telemetry (#3405)
    * Remove unneeded blob telemetry
    
    * safe
    
    * safe

--- src/Microsoft.VisualStudio.Services.Agent/Blob/BlobStoreClientTelemetryTfs.cs
+++ src/Microsoft.VisualStudio.Services.Agent/Blob/BlobStoreClientTelemetryTfs.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.Services.WebApi;
 using Microsoft.VisualStudio.Services.Content.Common.Tracing;
@@ -12,26 +13,21 @@ namespace Microsoft.VisualStudio.Services.Agent.Blob
 {
     public class BlobStoreClientTelemetryTfs : BlobStoreClientTelemetry
     {
-        private CustomerIntelligenceTelemetrySender _ciSender;
-
         public BlobStoreClientTelemetryTfs(IAppTraceSource tracer, Uri baseAddress, VssConnection connection) 
-            : base(tracer, baseAddress)
+            : base(tracer, baseAddress, new CustomerIntelligenceTelemetrySender(connection))
         {
-            _ciSender = new CustomerIntelligenceTelemetrySender(connection);
-            this.senders.Add(_ciSender);
         }
 
         // for testing
         public BlobStoreClientTelemetryTfs(IAppTraceSource tracer, Uri baseAddress, VssConnection connection, ITelemetrySender sender)
             : base(tracer, baseAddress, sender)
         {
-            _ciSender = new CustomerIntelligenceTelemetrySender(connection);
-            this.senders.Add(_ciSender);
         }
 
         public async Task CommitTelemetry(Guid planId, Guid jobId)
         {
-            await _ciSender.CommitTelemetry(planId, jobId);
+            var ciSender = this.senders.OfType<CustomerIntelligenceTelemetrySender>().FirstOrDefault();
+            await (ciSender?.CommitTelemetry(planId, jobId) ?? Task.CompletedTask);
         }
     }
 }
