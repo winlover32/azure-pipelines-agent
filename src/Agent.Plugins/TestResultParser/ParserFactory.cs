578b6a14c525823c7bd5349a978c83eb4acace7f
Anatoly Bolshakov <anatoly.bolshakov@akvelon.com>
2021-09-07 12:42:42 +0300

    Fixed error on finalize job (Could not load file or assembly 'Microsoft.VisualStudio.Services.Agent'...) (#3526)
    * Added error handling
    
    * Updated comment

--- src/Agent.Plugins/TestResultParser/ParserFactory.cs
+++ src/Agent.Plugins/TestResultParser/ParserFactory.cs
@@ -20,9 +20,20 @@ namespace Agent.Plugins.Log.TestResultParser.Plugin
 
             var interfaceType = typeof(AbstractTestResultParser);
             return AppDomain.CurrentDomain.GetAssemblies()
-                .SelectMany(x => x.GetTypes())
-                .Where(x => interfaceType.IsAssignableFrom(x) && !x.IsInterface && !x.IsAbstract)
-                .Select(x => (AbstractTestResultParser)Activator.CreateInstance(x, testRunManager, logger, telemetry));
+               .SelectMany((x) =>
+               {
+                   try
+                   {
+                       return x.GetTypes();
+                   }
+                   catch
+                   {
+                       // Skipping issues with assemblies load via reflection - since there are some issues with 'Microsoft.VisualStudio.Services.Agent', although this assembly is not relevant here
+                       return new Type[0];
+                   }
+               })
+             .Where(x => interfaceType.IsAssignableFrom(x) && !x.IsInterface && !x.IsAbstract)
+            .Select(x => (AbstractTestResultParser)Activator.CreateInstance(x, testRunManager, logger, telemetry));
         }
     }
 }
