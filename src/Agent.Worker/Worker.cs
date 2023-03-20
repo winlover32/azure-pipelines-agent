8114868972b622079c87e50a1d7d10fcddd50d68
Konstantin Tyukalov <52399739+KonstantinTyukalov@users.noreply.github.com>
2023-03-20 13:46:16 +0400

    Added trimmed user secret to masker dictionary (#4195)
    * Add trimmed secret value to masker dictionary

--- src/Agent.Worker/Worker.cs
+++ src/Agent.Worker/Worker.cs
@@ -138,6 +138,10 @@ namespace Microsoft.VisualStudio.Services.Agent.Worker
                     HostContext.SecretMasker.AddValue(secret.Trim(quoteChar), WellKnownSecretAliases.UserSuppliedSecret);
                 }
             }
+
+            // Here we add a trimmed secret value to the dictionary in case of a possible leak through external tools.
+            var trimChars = new char[] { '\r', '\n', ' ' };
+            HostContext.SecretMasker.AddValue(secret.Trim(trimChars), WellKnownSecretAliases.UserSuppliedSecret);
         }
 
         private void InitializeSecretMasker(Pipelines.AgentJobRequestMessage message)
