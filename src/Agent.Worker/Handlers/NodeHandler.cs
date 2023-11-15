aaf9fcd885e364d558c0ed7f027e2407f5640563
Kirill Ivlev <102740624+kirill-ivlev@users.noreply.github.com>
2023-11-15 14:09:07 +0400

    Revert "Remove VSTS-task-lib from agent (#4521)" (#4526)
    This reverts commit 941e9d4be525fd6b6c8db0b48905baf8a0fda648.

--- src/Agent.Worker/Handlers/NodeHandler.cs
+++ src/Agent.Worker/Handlers/NodeHandler.cs
@@ -60,6 +60,25 @@ namespace Microsoft.VisualStudio.Services.Agent.Worker.Handlers
         private const string useNodeKnobLtsKey = "LTS";
         private const string useNodeKnobUpgradeKey = "UPGRADE";
         private string[] possibleNodeFolders = { nodeFolder, node10Folder, Node16Folder, Node20_1Folder };
+        private static Regex _vstsTaskLibVersionNeedsFix = new Regex("^[0-2]\\.[0-9]+", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        private static string[] _extensionsNode6 ={
+            "if (process.versions.node && process.versions.node.match(/^5\\./)) {",
+            "   String.prototype.startsWith = function (str) {",
+            "       return this.slice(0, str.length) == str;",
+            "   };",
+            "   String.prototype.endsWith = function (str) {",
+            "       return this.slice(-str.length) == str;",
+            "   };",
+            "};",
+            "String.prototype.isEqual = function (ignoreCase, str) {",
+            "   var str1 = this;",
+            "   if (ignoreCase) {",
+            "       str1 = str1.toLowerCase();",
+            "       str = str.toLowerCase();",
+            "       }",
+            "   return str1 === str;",
+            "};"
+        };
 
         public NodeHandler()
         {
@@ -81,6 +100,19 @@ namespace Microsoft.VisualStudio.Services.Agent.Worker.Handlers
             ArgUtil.NotNull(ExecutionContext, nameof(ExecutionContext));
             ArgUtil.NotNull(Inputs, nameof(Inputs));
             ArgUtil.Directory(TaskDirectory, nameof(TaskDirectory));
+
+            if (!PlatformUtil.RunningOnWindows && !AgentKnobs.IgnoreVSTSTaskLib.GetValue(ExecutionContext).AsBoolean())
+            {
+                // Ensure compat vso-task-lib exist at the root of _work folder
+                // This will make vsts-agent work against 2015 RTM/QU1 TFS, since tasks in those version doesn't package with task lib
+                // Put the 0.5.5 version vso-task-lib into the root of _work/node_modules folder, so tasks are able to find those lib.
+                if (!File.Exists(Path.Combine(HostContext.GetDirectory(WellKnownDirectory.Work), "node_modules", "vso-task-lib", "package.json")))
+                {
+                    string vsoTaskLibFromExternal = Path.Combine(HostContext.GetDirectory(WellKnownDirectory.Externals), "vso-task-lib");
+                    string compatVsoTaskLibInWork = Path.Combine(HostContext.GetDirectory(WellKnownDirectory.Work), "node_modules", "vso-task-lib");
+                    IOUtil.CopyDirectory(vsoTaskLibFromExternal, compatVsoTaskLibInWork, ExecutionContext.CancellationToken);
+                }
+            }
 
             // Update the env dictionary.
             AddInputsToEnvironment();
@@ -105,6 +137,23 @@ namespace Microsoft.VisualStudio.Services.Agent.Worker.Handlers
                 {
                     workingDirectory = HostContext.GetDirectory(WellKnownDirectory.Work);
                 }
+            }
+
+            // fix vsts-task-lib for node 6.x
+            // vsts-task-lib 0.6/0.7/0.8/0.9/2.0-preview implemented String.prototype.startsWith and String.prototype.endsWith since Node 5.x doesn't have them.
+            // however the implementation is added in node 6.x, the implementation in vsts-task-lib is different.
+            // node 6.x's implementation takes 2 parameters str.endsWith(searchString[, length]) / str.startsWith(searchString[, length])
+            // the implementation vsts-task-lib had only takes one parameter str.endsWith(searchString) / str.startsWith(searchString).
+            // as long as vsts-task-lib be loaded into memory, it will overwrite the implementation node 6.x has,
+            // so any script that use the second parameter (length) will encounter unexpected result.
+            // to avoid customer hit this error, we will modify the file (extensions.js) under vsts-task-lib module folder when customer choose to use Node 6.x
+            if (!AgentKnobs.IgnoreVSTSTaskLib.GetValue(ExecutionContext).AsBoolean())
+            {
+                Trace.Info("Inspect node_modules folder, make sure vsts-task-lib doesn't overwrite String.startsWith/endsWith.");
+                FixVstsTaskLibModule();
+            } else
+            {
+                Trace.Info("AZP_AGENT_IGNORE_VSTSTASKLIB enabled, ignoring fix");
             }
 
             StepHost.OutputDataReceived += OnDataReceived;
@@ -321,6 +370,56 @@ namespace Microsoft.VisualStudio.Services.Agent.Worker.Handlers
                 return false;
             }
             return true;
+        }
+
+        private void FixVstsTaskLibModule()
+        {
+            // to avoid modify node_module all the time, we write a .node6 file to indicate we finsihed scan and modify.
+            // the current task is good for node 6.x
+            if (File.Exists(TaskDirectory.TrimEnd(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar) + ".node6"))
+            {
+                Trace.Info("This task has already been scanned and corrected, no more operation needed.");
+            }
+            else
+            {
+                Trace.Info("Scan node_modules folder, looking for vsts-task-lib\\extensions.js");
+                try
+                {
+                    foreach (var file in new DirectoryInfo(TaskDirectory).EnumerateFiles("extensions.js", SearchOption.AllDirectories))
+                    {
+                        if (string.Equals(file.Directory.Name, "vsts-task-lib", StringComparison.OrdinalIgnoreCase) ||
+                            string.Equals(file.Directory.Name, "vso-task-lib", StringComparison.OrdinalIgnoreCase))
+                        {
+                            if (File.Exists(Path.Combine(file.DirectoryName, "package.json")))
+                            {
+                                // read package.json, we only do the fix for 0.x->2.x
+                                JObject packageJson = JObject.Parse(File.ReadAllText(Path.Combine(file.DirectoryName, "package.json")));
+
+                                JToken versionToken;
+                                if (packageJson.TryGetValue("version", StringComparison.OrdinalIgnoreCase, out versionToken))
+                                {
+                                    if (_vstsTaskLibVersionNeedsFix.IsMatch(versionToken.ToString()))
+                                    {
+                                        Trace.Info($"Fix extensions.js file at '{file.FullName}'. The vsts-task-lib version is '{versionToken.ToString()}'");
+
+                                        // take backup of the original file
+                                        File.Copy(file.FullName, Path.Combine(file.DirectoryName, "extensions.js.vstsnode5"));
+                                        File.WriteAllLines(file.FullName, _extensionsNode6);
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    File.WriteAllText(TaskDirectory.TrimEnd(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar) + ".node6", string.Empty);
+                    Trace.Info("Finished scan and correct extensions.js under vsts-task-lib");
+                }
+                catch (Exception ex)
+                {
+                    Trace.Error("Unable to scan and correct potential bug in extensions.js of vsts-task-lib.");
+                    Trace.Error(ex);
+                }
+            }
         }
     }
 }
